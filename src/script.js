0d22ad7f423b00a4c01be0cd4bd072d4bcbedf8a
Tuur Dutoit <me@tuurdutoit.be>
2015-05-03 21:05:04 +0200

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,16 @@
+/*******************
+ * Test vh support *
+ *******************/
+
+(function() {
+    var div = X("<div>").get(0);
+    div.width = "";
+    div.width = "10vh";
+    
+    if(!div.width) {
+        X("head").appendChild( X("<style>").html("html,body{height:100%;}.pane{height:100%;}") );
+    }
+}());
+
+
+
